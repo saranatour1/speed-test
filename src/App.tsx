306428df776dd72b1450db261f52c6872e639bd7
saranatour1 <saranatourcontact@gmail.com>
2025-12-26 11:10:54 +0200

    Initial design system

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// @ts-expect-error okay
+import '@fontsource-variable/sora';
+
+
+function App() {
+
+  return (
+    <p className=''>Hello we're just checking that this is working</p>
+  )
+}
+
+export default App
